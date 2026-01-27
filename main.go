dfcd4a37ce29788bfe28b2c87d4c31941236ebf3
AyumuOgasawara <ogasaayumu0114@gmail.com>
2026-01-28 01:45:24 +0900

    Add made entry point

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	// Load configuration from environment variables
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	// Fetch news
+	news, err := getNews(config.RSSURL)
+	if err != nil {
+		log.Fatalf("Failed to get news: %v", err)
+	}
+
+	// Format messages
+	messages := FormatHackerNews(news)
+
+	// Send LINE messages
+	client := NewLineClient(config.LineAPIURL, config.LineAccessToken, config.TargetUserID)
+	err = sendBatchLineMessage(client, messages)
+	if err != nil {
+		log.Fatalf("Failed to send LINE message: %v", err)
+	}
+
+	log.Println("Successfully sent messages")
+}
